12ee1b759a579527fdaec7661722e8bd910c865c
rosejam268 <marijo268@gmail.com>
2025-11-05 19:44:47 -0300

    Add allowedHosts config for Netlify previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        server: {
+            allowedHosts: [
+                "devserver-preview--sonrisologos.netlify.app",
+                "devserver-preview--neon-pixie-d3aa91.netlify.app"
+            ]
+        }
     },
     integrations: [react()],
     adapter: netlify({
